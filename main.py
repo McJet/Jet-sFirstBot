957e37a34b489a14cb58010bda6a313789f6f6bb
Jether (Jet) Ysalina <>
2021-05-17 20:19:49 +0000

    users can add/delete messages from the bot

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import requests
 import json
 import random
+from replit import db
+
 
 # gets a quote from zen quotes api - https://zenquotes.io/
 def get_quote():
@@ -11,6 +13,19 @@ def get_quote():
     joke = json_data[0]['q'] + "\n- " + json_data[0]['a']
     return joke
 
+def update_encouragements(encouragementMsg):
+    if "encouragements" in db.keys():
+        encouragements = db["encouragements"]
+        encouragements.append(encouragementMsg)
+        db["encouragement"] = encouragements
+    else:
+        db["encouragements"] = [encouragementMsg]
+
+def delete_encouragement(index):
+    encouragements = db["encouragements"]
+    if len(encouragements) > index:
+        del encouragements[index]
+    db["encouragements"] = encouragements
 
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
 starter_encouragement = [
@@ -35,7 +50,33 @@ async def on_message(message): # triggers every time a message is recieved
         quote = get_quote()
         await message.channel.send(quote)
 
+    options = starter_encouragement
+    if "encouragements" in db.keys():
+        options.extend(db["encouragements"])
+
     if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragement))
+        await message.channel.send(random.choice(options))
+
+    if msg.startswith("$new"):
+        encouragingMsg = msg.split("$new ", 1) [1]
+        update_encouragements(encouragingMsg)
+        await message.channel.send("New encouragement added.")
+    
+    if msg.startswith("$del"):
+        encouragements = []
+        if "encouragements" in db.keys():
+            index = int(msg.split("$del ", 1) [1])
+            delete_encouragement(index)
+            encouragements = db["encouragements"]
+        await message.channel.send(encouragements)
+
+        update_encouragements(encouragingMsg)
+        await message.channel.send("New encouragement added.")
+    
+    if msg.startswith("$list"):
+        encouragements = []
+        if "encouragements" in db.keys():
+            encouragements = db["encouragements"]
+        await message.channel.send(encouragements)
 
 client.run(os.environ['TOKEN'])  # runs the bot
